be8127a769260b5674863492456a3ab9c71bc522
lhornby <lukas.hornby@gmail.com>
2014-01-28 10:37:39 +0000

    Update main.js
    Added function to render html to display homepage

--- js/main.js
+++ js/main.js
@@ -17,9 +17,20 @@ var app = {
         var self = this;
         this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
+    
+    renderHomeView: function() {
+        var html = 
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='employee-list'></ul>" +
+            "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
     
     showAlert: function (message, title) {
         if(navigator.notification) {
