c9dd6283e4479bcd81fef1f358faa01b9c0f2410
lhornby <lukas.hornby@gmail.com>
2014-01-28 11:11:38 +0000

    Update main.js
    Modify functions to use HTML templates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
@@ -17,19 +11,15 @@ var app = {
         var self = this;
         this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
+            this.homeTpl = Handlebars.compile($("#home-tpl").html());
+            this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
             self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
     
     renderHomeView: function() {
-        var html = 
-            "<div class='header'><h1>Home</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'></ul>" +
-            "</div>"
-        $('body').html(html);
+        $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
     
