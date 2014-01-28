ffe786e995eab97c29cf303c6f696b20f03b1745
lhornby <lukas.hornby@gmail.com>
2014-01-28 11:51:28 +0000

    Update main.js
    Removed functions that have been placed in Homeview.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,12 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
+  
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
-            this.homeTpl = Handlebars.compile($("#home-tpl").html());
-            this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-    
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
     
     showAlert: function (message, title) {
