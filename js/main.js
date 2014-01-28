146e0dc55371cf8b26dd18f719ea711be11c9f9c
lhornby <lukas.hornby@gmail.com>
2014-01-28 10:51:05 +0000

    Update main.js
    Added missing bits of html

--- js/main.js
+++ js/main.js
@@ -26,11 +26,12 @@ var app = {
         var html = 
             "<div class='header'><h1>Home</h1></div>" +
             "<div class='search-view'>" +
-            "<input class='employee-list'></ul>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
             "</div>"
         $('body').html(html);
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
     
     showAlert: function (message, title) {
         if(navigator.notification) {
