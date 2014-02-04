6921c6cd0a5b8afd41b2a957a8c1b058b68f57df
lhornby <lukas.hornby@gmail.com>
2014-02-04 22:02:17 +0000

    Update EmployeeView.js

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -28,7 +28,7 @@ var EmployeeView = function(employee) {
   this.addToContacts = function(event) {
     event.preventDefault();
     console.log('addToContacts');
-    if(!navigator.contacts) {
+    if (!navigator.contacts) {
       app.showAlert("Contacts API not supported", "Error");
       return;
     }
@@ -44,15 +44,16 @@ var EmployeeView = function(employee) {
   
   this.changePicture = function(event) {
     event.preventDefault();
-    if(!navigator.camera) {
-      app.showAlert("camera API not supported", "Error");
+    if (!navigator.camera) {
+      app.showAlert("Camera API not supported", "Error");
       return;
     }
     var options = { quality: 50,
                     destinationType: Camera.DetinationType.DATA_URL,
                     sourceType: 1,    //0:Photo Library, 1=Camera, 2=Saved Photo Album
                     encodingType: 0   //0=JPG 1=PNG
-    };   
+                  };
+                  
     navigator.camera.getPicture(
       function(imageData) {
         $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
@@ -61,6 +62,7 @@ var EmployeeView = function(employee) {
         app.showAlert('Error taking picture', 'Error');
       },
       options);
+      
     return false;
   };
   
